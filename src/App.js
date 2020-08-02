3d264c1ca968810e05ed404fbfbaa3351908e9b0
prasadcl <prasadclwork@gmail.com>
2020-08-02 22:53:04 +0530

    update 5 header component added links to shop and contact added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import './App.css';
 //
 import HomePage from './Pages/homepage/homepage.component';
 import ShopPage from './Pages/shop/shop.component';
+import Header from './Components/header/header.component';
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
