4f678379fe33d3e8fd52c930bfff5038c996dcf8
prasadcl <prasadclwork@gmail.com>
2020-08-02 22:21:46 +0530

    update 4 shop page added and collection preview and collection item added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import './App.css';
 
 //
 import HomePage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
