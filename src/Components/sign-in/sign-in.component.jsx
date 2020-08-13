a82476265ff8f49b9cbe3355f95bfd2e946fe0ec
prasadcl <prasadclwork@gmail.com>
2020-08-13 21:02:32 +0530

    added firebase signin and signup

--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -8,7 +8,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle } from "../../firebase/firebase.utils";
 
 
 
@@ -22,13 +22,23 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async event => {
             event.preventDefault();
 
-            this.setState = ({
-                email: '',
-                password: ''
-            })
+            const { email , password} = this.state;
+
+            try {
+                await auth.signInWithEmailAndPassword(email,password);
+                this.setState = ({
+                    email: '',
+                    password: ''
+                })
+
+            }catch (error) {
+                console.log(error);
+            }
+
+            
         };
 
         handleChange = (event) => {
